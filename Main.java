f2908621a9f59821d1b1669900f7167c6bcdcd3b
Subhajit Kanungo <subhajitinfospace.com@gmail.com>
2026-09-20 19:29:37 +0530

    Add main Code

---
+++ Main.java
@@ -0,0 +1,63 @@
+abstract class Shape {
+    public abstract double calculateArea();
+}
+
+// Circle subclass
+class Circle extends Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+}
+
+// Rectangle subclass
+class Rectangle extends Shape {
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    public double calculateArea() {
+        return length * width;
+    }
+}
+
+// Triangle subclass
+class Triangle extends Shape {
+    private double base;
+    private double height;
+
+    public Triangle(double base, double height) {
+        this.base = base;
+        this.height = height;
+    }
+
+    @Override
+    public double calculateArea() {
+        return 0.5 * base * height;
+    }
+}
+
+// Main class
+public class Main {
+    public static void main(String[] args) {
+
+        Shape circle = new Circle(5);
+        Shape rectangle = new Rectangle(6, 4);
+        Shape triangle = new Triangle(8, 3);
+
+        System.out.println("Area of Circle: " + circle.calculateArea());
+        System.out.println("Area of Rectangle: " + rectangle.calculateArea());
+        System.out.println("Area of Triangle: " + triangle.calculateArea());
+    }
+}
